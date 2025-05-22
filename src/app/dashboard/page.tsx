e828246ab35854c3453690e7331bedb7e5a3c1fc
mariocapitbrok <mario@capitbrok.com>
2025-05-21 19:44:27 -0600

    Create Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
-export default function Dashboard() {
+// app/page.tsx
+
+export default function Home() {
   return (
-    <div className="">
-      <h1 className="">Dashboard</h1>
-      <p>Welcome to your CRM Dashboard!</p>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Contents...</p>
     </div>
   );
 }
